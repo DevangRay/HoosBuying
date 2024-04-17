d0403f5a339bf046fbc94dddcb09eebeca57b237
Devang Ray <devangray624@gmail.com>
2024-04-16 21:26:30 -0400

    set up basic routing

---
+++ frontend/HoosBuying/src/views/ListingView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="listing">
+    <h1>This is a listing page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .listing {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
