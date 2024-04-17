2b483a8f2f5df2fcec3c680de045d30623f841aa
Devang Ray <devangray624@gmail.com>
2024-04-17 01:51:08 -0400

    calling storedProcedure actually edits table now!

--- backend/db_users.py
+++ backend/db_users.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
 def connect():
     # Please fill in these values.
     # project_id = "cs4750db-413417"
@@ -19,6 +20,7 @@ def connect():
     )
     print("CONNECTION TO RETURN IS", connection)
     return connection
+
 
 @app.route('/getAllUsers', methods=['GET'])
 def getAllUsers():
@@ -39,53 +41,37 @@ def getAllUsers():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+
 @app.route('/testPostToken', methods=['GET'])
 def testPostToken():
     dictToSend = {"token": "good"}
     res = requests.post("http://127.0.0.1:5000/postToken", json=dictToSend)
-    # print("response from server is", res.text)
-    if res.text == "Success":
-        return "successful token", 200
+    # print("response from server is", res, res.status_code)
+    if res.status_code == 200:
+        return "successful get token", 200
     else:
-        return "bad token", 401
+        return "bad get token", 401
     
-@app.route('/checkToken', methods=['POST'])
+    
+@app.route('/postToken', methods=['POST'])
 def checkToken():
     token = request.json['token']
     print("token issss", token)
-    connection = connect()
-    result = "GOT NOTHING PAL"
-    with connection:
-        with connection.cursor() as cursor:
-        # Create a new record
-            cursor.execute("CALL auth_select(\"" + token + "\")")
-            result = cursor.fetchall()
-            
-    if not result:
-        return "bad token", 401
-    else:
-        return "Success token", 200
-
-@app.route('/getToken', methods=['GET'])
-def getToken():
-    connection = connect()
-    result = "GOT NOTHING PAL"
-    with connection:
-        with connection.cursor() as cursor:
-        # Create a new record
-            cursor.execute("CALL auth_select(\"good\")")
-            result = cursor.fetchall()
-            # print("RESULT is ", result)
-
-    # connection is not autocommit by default. So you must commit to save
-    # your changes.
-    # connection.commit()
-    if not result:
-        return abort(401)
-    else:
-        return "Success", 200
     
+    connection = connect()
+    result = "GOT NOTHING PAL"
+    with connection:
+        with connection.cursor() as cursor:
+        # Create a new record
+            cursor.callproc('auth_select', [token,])
+            result = cursor.fetchall()
+        
+    if not result:
+        return "bad post token", 401
+    else:
+        return "Success post token", 200
 
+    
 if __name__ == '__main__':
    app.run(port=5000)
 #    print("IN MAIN")
