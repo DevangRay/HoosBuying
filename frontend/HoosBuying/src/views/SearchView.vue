d8328004d5937190d16eab102620011c5ca67b30
Devang Ray <devangray624@gmail.com>
2024-04-21 23:19:43 -0400

    logos show up

--- frontend/HoosBuying/src/views/SearchView.vue
+++ frontend/HoosBuying/src/views/SearchView.vue
@@ -1,27 +1,31 @@
 <template>
-      <div class="search"></div>
-        <h1>This is a search page</h1>
-          <div v-for="listing in result" :key="listing.listing_id">
-            <v-col>
-              <v-card>
-                <v-card-item>
-                  <v-card-title>{{listing.title}}</v-card-title>
-
+          <div>
+            <div v-for="listing in result" :key="listing.listing_id">
+              <v-col>
+                <v-card>
+                  <v-card-item>
+                    <v-card-title>{{listing.title}}</v-card-title>
                     <v-card-subtitle>${{ listing.price }}</v-card-subtitle>
-                </v-card-item>
-    
-                <v-card-text>
-                  {{ listing.description }}
-                </v-card-text>
-                <v-icon color="error" icon="mdi-cart"></v-icon>
-                <v-icon color="success" icon="mdi-account-group"></v-icon>
-                <v-card-item>
-                  {{listing.status_id}} : {{listing.status_name}}
-                  
-                  {{listing.delivery_id}} : {{listing.method_name}}
-                </v-card-item>
-              </v-card>
-            </v-col>
+                  </v-card-item>
+
+                  <v-card-item>
+                    Sale Status: {{listing.status_name}}
+
+                    <v-icon v-if="listing.status_id==1" size="x-large" color="success" icon="mdi-cart"></v-icon>
+                    <v-icon v-if="listing.status_id==2" size="x-large" color="warning" icon="mdi-cart"></v-icon>
+                    <v-icon v-if="listing.status_id==3" size="x-large" color="error" icon="mdi-cart"></v-icon>
+                  </v-card-item>
+                  <!-- <v-icon color="success" icon="mdi-account-group"></v-icon> -->
+                  <v-card-item>
+                    Preferred Method of Delivery: {{listing.method_name}}
+
+                    <v-icon v-if="listing.delivery_id==1" size="x-large" color="info" icon="mdi-account-group"></v-icon>
+                    <v-icon v-if="listing.delivery_id==2" size="x-large" color="info" icon="mdi-email-fast"></v-icon>
+                    <v-icon v-if="listing.delivery_id==3" size="x-large" color="info" icon="mdi-truck-delivery"></v-icon>
+                  </v-card-item>
+                </v-card>
+              </v-col>
+            </div>
           </div>
 </template>
 
