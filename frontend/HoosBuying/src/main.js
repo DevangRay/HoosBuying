0c7d2538d58dd983df4fa0fd542601135951d2be
Devang Ray <devangray624@gmail.com>
2024-04-21 21:15:02 -0400

    merging dev in

--- frontend/HoosBuying/src/main.js
+++ frontend/HoosBuying/src/main.js
@@ -7,7 +7,6 @@ import App from './App.vue'
 import router from './router'
 import store from './stores';
 import axios from 'axios';
-
 const app = createApp(App)
 axios.defaults.baseURL = "http://127.0.0.1:5000/";
 
