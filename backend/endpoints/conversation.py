8486667e6ed8d70a24059daff1011cdfe9b5742f
vtarashansky <vladtarashansky@gmail.com>
2024-04-28 19:10:39 -0400

    chatlist working

--- backend/endpoints/conversation.py
+++ backend/endpoints/conversation.py
@@ -13,7 +13,7 @@ def getAllConvoIds(user_id):
     with connection:
         with connection.cursor() as cursor:
         # Create a new record
-            cursor.execute("SELECT c.*, u.username as host_user, u.fname as host_fname, u.lname as host_lname, u.computing_id as host_cid, u2.username as cust_user, u2.fname as cust_fname, u2.lname as cust_lname, u2.computing_id as cust_cid, l.title, l.price, l.status_id , s.status_name FROM Conversation c join `User` u on c.host_id = u.uid join `User` u2 on c.customer_id =u2.uid join Listings l on c.listing_id = l.listing_id  join Status s on l.status_id = s.status_id WHERE host_id = %s OR customer_id = %s;", (user_id, user_id, ))
+            cursor.execute("SELECT c.*, u.username as host_user, u.fname as host_fname, u.lname as host_lname, u.computing_id as host_cid, u2.username as cust_user, u2.fname as cust_fname, u2.lname as cust_lname, u2.computing_id as cust_cid, l.title, l.price, l.status_id , s.status_name FROM Conversation c join `User` u on c.host_id = u.uid join `User` u2 on c.customer_id =u2.uid join Listings l on c.listing_id = l.listing_id  join Status s on l.status_id = s.status_id WHERE host_id = %s OR customer_id = %s order by c.last_updated DESC;", (user_id, user_id, ))
             result = cursor.fetchall()
     return result
     # print(type(result))
@@ -29,7 +29,7 @@ def getAllOwnerConvoIds(user_id):
     with connection:
         with connection.cursor() as cursor:
         # Create a new record
-            cursor.execute("SELECT c.*, u.username as host_user, u.fname as host_fname, u.lname as host_lname, u.computing_id as host_cid, u2.username as cust_user, u2.fname as cust_fname, u2.lname as cust_lname, u2.computing_id as cust_cid, l.title, l.price, l.status_id , s.status_name FROM Conversation c join `User` u on c.host_id = u.uid join `User` u2 on c.customer_id =u2.uid join Listings l on c.listing_id = l.listing_id  join Status s on l.status_id = s.status_id WHERE host_id = %s;", (user_id, ))
+            cursor.execute("SELECT c.*, u.username as host_user, u.fname as host_fname, u.lname as host_lname, u.computing_id as host_cid, u2.username as cust_user, u2.fname as cust_fname, u2.lname as cust_lname, u2.computing_id as cust_cid, l.title, l.price, l.status_id , s.status_name FROM Conversation c join `User` u on c.host_id = u.uid join `User` u2 on c.customer_id =u2.uid join Listings l on c.listing_id = l.listing_id  join Status s on l.status_id = s.status_id WHERE host_id = %s order by c.last_updated DESC;", (user_id, ))
             result = cursor.fetchall()
     return result
 
@@ -39,7 +39,7 @@ def getAllCustomerConvoIds(user_id):
     with connection:
         with connection.cursor() as cursor:
         # Create a new record
-            cursor.execute("SELECT c.*, u.username as host_user, u.fname as host_fname, u.lname as host_lname, u.computing_id as host_cid, u2.username as cust_user, u2.fname as cust_fname, u2.lname as cust_lname, u2.computing_id as cust_cid, l.title, l.price, l.status_id , s.status_name FROM Conversation c join `User` u on c.host_id = u.uid join `User` u2 on c.customer_id =u2.uid join Listings l on c.listing_id = l.listing_id  join Status s on l.status_id = s.status_id WHERE customer_id = %s;", (user_id, ))
+            cursor.execute("SELECT c.*, u.username as host_user, u.fname as host_fname, u.lname as host_lname, u.computing_id as host_cid, u2.username as cust_user, u2.fname as cust_fname, u2.lname as cust_lname, u2.computing_id as cust_cid, l.title, l.price, l.status_id , s.status_name FROM Conversation c join `User` u on c.host_id = u.uid join `User` u2 on c.customer_id =u2.uid join Listings l on c.listing_id = l.listing_id  join Status s on l.status_id = s.status_id WHERE customer_id = %s order by c.last_updated DESC;", (user_id, ))
             result = cursor.fetchall()
     return result
 
@@ -49,7 +49,7 @@ def getConversationObject(user_id, convo_arr):
         with connection.cursor() as cursor:
             # Create a new record
             for i in range(len(convo_arr)):
-                cursor.execute("select * from Message m where convo_id = %s", (convo_arr[i]["convo_id"], ))
+                cursor.execute("select * from Message m where convo_id = %s order by time DESC;", (convo_arr[i]["convo_id"], ))
                 message_arr = cursor.fetchall()
                 convo_arr[i]["message_array"] = message_arr
                 
--- frontend/HoosBuying/src/App.vue
+++ frontend/HoosBuying/src/App.vue
@@ -6,61 +6,59 @@ import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <v-layout light>
-  <v-app-bar :elevation="2">
-    <template v-slot:prepend>
-      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
-    </template>
+  <v-container fluid>
+    <v-layout light>
+      <v-app-bar :elevation="2">
+        <template v-slot:prepend>
+          <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+        </template>
 
-    <v-app-bar-title>HoosBuying</v-app-bar-title>
-    <v-spacer></v-spacer>
-      <v-app-bar-items class="hidden-xs-only">
-        <v-btn
-          flat
-          v-for="item in menuItems"
-          :key="item.title"
-          :to="item.path">
-          <v-icon left dark>{{ item.icon }}</v-icon>
-          {{ item.title }}
-        </v-btn>
-      </v-app-bar-items>
-  </v-app-bar>
-  <v-main>
- 
-  <!-- <h1>HoosBuying</h1> -->
-  <!-- <NavBar></NavBar> -->
-      
-  
-  
-  
-    
-  <v-container >
-    <RouterView />
+        <v-app-bar-title>HoosBuying</v-app-bar-title>
+        <v-spacer></v-spacer>
+        <v-app-bar-items class="hidden-xs-only">
+          <v-btn flat v-for="item in menuItems" :key="item.title" :to="item.path">
+            <v-icon left dark>{{ item.icon }}</v-icon>
+            {{ item.title }}
+          </v-btn>
+        </v-app-bar-items>
+      </v-app-bar>
+      <v-main>
+
+        <!-- <h1>HoosBuying</h1> -->
+        <!-- <NavBar></NavBar> -->
+
+
+
+
+
+        <v-container fluid>
+          <RouterView />
+        </v-container>
+      </v-main>
+    </v-layout>
   </v-container>
-  </v-main>
-</v-layout>
 </template>
 <script>
 //import HelloWorld from "./components/HelloWorld";
 
 export default {
   name: "App",
-  data(){
+  data() {
     return {
       appTitle: 'Awesome App',
       sidebar: false,
       menuItems: [
-          { title: 'Home', path: '/', icon: 'mdi-home' },
-          { title: 'Search', path: '/search', icon: 'mdi-card-search' },
-          { title: 'Chats', path: '/chats', icon: 'mdi-forum' },
-          { title: 'Account', path: '/account', icon: 'mdi-account-cowboy-hat-outline' },
-          { title: 'Logout', path: '/logout', icon: 'mdi-exit-run' },
-     ]
+        { title: 'Home', path: '/', icon: 'mdi-home' },
+        { title: 'Search', path: '/search', icon: 'mdi-card-search' },
+        { title: 'Chats', path: '/chats', icon: 'mdi-forum' },
+        { title: 'Account', path: '/account', icon: 'mdi-account-cowboy-hat-outline' },
+        { title: 'Logout', path: '/logout', icon: 'mdi-exit-run' },
+      ]
     }
   },
   name: "App",
   computed: {
-    isLoggedIn: function() {
+    isLoggedIn: function () {
       return this.$store.getters.isAuthenticated;
     },
   },
@@ -74,8 +72,4 @@ export default {
 </script>
 
 
-<style>
-</style>
-
-
-
+<style></style>
